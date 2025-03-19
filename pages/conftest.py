fdc3e8e02da3147f2a4c42c684d31db7318223a0
Zoey <dazam1@naver.com>
2025-03-19 11:55:49 +0900

    setting

---
+++ pages/conftest.py
@@ -0,0 +1,12 @@
+@pytest.fixture(scope="function")
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-extensions') 
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--disable-popup-blocking')
+
+    driver = webdriver.Chrome(service=Service(), options=chrome_options)
+
+    yield driver
+
+    driver.quit()
