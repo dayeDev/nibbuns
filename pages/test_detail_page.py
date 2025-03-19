ff406e1a1a56b6edc47ec43b97e7391e7cafdebf
won95kim <hw95.kim@gmail.com>
2025-03-19 22:32:58 +0900

    [UPDATE] detail_page
    기능 코드 수정 및 추가
    테스트 코드 수정 및 추가

--- pages/test_detail_page.py
+++ pages/test_detail_page.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait as ws
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 # 상위 디렉토리로 이동 후 src를 경로에 추가
@@ -15,16 +16,15 @@ from src.detail_page import DetailPage
 
 @pytest.mark.usefixtures("driver")
 class TestDetailPage:
-    BRAND_ID = "69560"
-    OPTION = "실버"
+    BRAND_ID = "70015"
+    OPTION = "아이보리"
     OPTION_NAME = "MK_p-name"
-
 
     # 상품 상세페이지 진입 테스트
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_open_detail_page(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -48,7 +48,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_select_option(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -81,7 +81,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_buy_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 로그인 진행
@@ -127,7 +127,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_cart_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 로그인 진행
@@ -170,7 +170,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_detail_goods_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -205,7 +205,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_detail_review_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -240,7 +240,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_detail_qna_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -272,10 +272,10 @@ class TestDetailPage:
 
     
     # 관련상품 탭 클릭 테스트
-    #@pytest.mark.skip(reason="테스트 케이스 SKIP")
+    @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_detail_relation_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -290,7 +290,7 @@ class TestDetailPage:
             # 관련상품 탭 클릭
             detail_page.detail_relation_click()
 
-            time.sleep(4)
+            time.sleep(3)
 
             # 눌렸는지 확인
             active_tab = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, detail_page.DETAIL_RELATION_EL + ".active")))
@@ -310,7 +310,7 @@ class TestDetailPage:
     @pytest.mark.skip(reason="테스트 케이스 SKIP")
     def test_detail_change_click(self, driver: WebDriver):
         detail_page = DetailPage(driver)
-        wait = ws(driver, 10)
+        wait = ws(driver, 20)
 
         try:
             # 상품 상세페이지 진입
@@ -339,4 +339,60 @@ class TestDetailPage:
         
         except Exception as e:
             assert False
+
+
+    
+    # 상품 하단 스크롤 후 바로 구매 버튼 클릭 테스트 (로그인 기준)
+    @pytest.mark.skip(reason="테스트 케이스 SKIP")
+    def test_fixed_buy_click(self, driver: WebDriver):
+        detail_page = DetailPage(driver)
+        wait = ws(driver, 20)
+
+        try:
+            # 로그인 진행
+            detail_page.login()
+
+            # 상품 상세페이지 진입
+            detail_page.open()
+
+            # 상품 상세페이지 정상 진입하였는지 확인
+            wait.until(EC.url_contains(self.BRAND_ID))
+            assert self.BRAND_ID in driver.current_url
+
+            time.sleep(1)
+
+            # 상품 옵션 선택
+            detail_page.select_option(self.OPTION)
+
+            # 상품 옵션 정상 선택되었는지 확인
+            option_check = wait.until(EC.presence_of_element_located((By.CLASS_NAME, self.OPTION_NAME)))
+            assert self.OPTION == option_check.text
+
+            time.sleep(2)
+
+            # 스크롤 후 바로 구매 버튼 클릭
+            detail_page.fixed_buy_click()
+
+            info_fixed = wait.until(EC.presence_of_element_located((By.ID, "info")))
+            assert info_fixed.is_displayed()
+
+            time.sleep(2)
+
+            # 바로 구매 버튼 클릭
+            detail_page.buy_click()
+
+            # alert 발생 시 확인 클릭 (장바구니에 이미 상품이 있을 시 발생)
+            detail_page.handle_alert()
+
+            # 구매 페이지 정상 진입하였는지 확인
+            wait.until(EC.url_contains("order"))
+            assert "order" in driver.current_url
+
+        except NoSuchElementException as e:
+            assert False
         
+        except TimeoutException as e:
+            assert False
+        
+        except Exception as e:
+            assert False
